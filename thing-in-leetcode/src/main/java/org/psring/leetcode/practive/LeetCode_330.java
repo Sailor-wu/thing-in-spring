ebcee7e0c4de5251790a152a32d7cdd3b87668b6
Sailor-wu <2856346101@qq.com>
2020-12-29 18:07:56 +0800

    leetcode 330.

---
+++ thing-in-leetcode/src/main/java/org/psring/leetcode/practive/LeetCode_330.java
@@ -0,0 +1,27 @@
+package org.psring.leetcode.practive;
+
+public final class LeetCode_330 {
+
+	public int minPatches(int[] nums, int n) {
+		int patches = 0;
+		long x = 1;
+		int length = nums.length, index = 0;
+		while (x <= n) {
+			if (index < length && nums[index] <= x) {
+				x += nums[index];
+				index++;
+			} else {
+				x *= 2;
+				patches++;
+			}
+		}
+		return patches;
+	}
+	
+	public static void main(String[] args) {
+		int nums [] = {1,3};
+		int n = 6;
+		LeetCode_330 code_330 = new LeetCode_330();
+		code_330.minPatches(nums, n);
+	}
+}
