8a6d1f58f4fec72322e2a96c0189d5351777c4e0
Sailor-wu <2856346101@qq.com>
2020-12-31 09:55:11 +0800

    设计模式模块。

---
+++ thing-in-spring-designmode/src/main/java/org/thing/in/spring/designmode/App.java
@@ -0,0 +1,16 @@
+package org.thing.in.spring.designmode;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
