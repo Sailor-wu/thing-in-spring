4149f99403b2d3eb6273efdd90121972aad2101a
Sailor-wu <2856346101@qq.com>
2021-04-13 11:23:14 +0800

    数组创建树节点。

--- thing-in-leetcode/src/main/java/org/psring/leetcode/practive/util/TreeNode.java
+++ thing-in-leetcode/src/main/java/org/psring/leetcode/practive/util/TreeNode.java
@@ -1,14 +1,17 @@
 package org.psring.leetcode.practive.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
-	public int val;
+	public Integer val;
 	public TreeNode left;
 	public TreeNode right;
-	public int[] nums;
+	public Integer[] nums;
 	public TreeNode() {
 	}
 
-	public TreeNode(int val) {
+	public TreeNode(Integer val) {
 		this.val = val;
 	}
 
@@ -19,16 +22,31 @@ public class TreeNode {
 	}
 	
 	
-	public  TreeNode createTreeNode(int [] arr) {
+	public  TreeNode createTreeNode(Integer [] arr) {
 		nums = arr;
         return helper(0,nums.length-1);
 	}
 	
 	private TreeNode helper(int begin,int end){
         if(begin > end) return null;
-        TreeNode root = new  TreeNode(nums[(begin+end)/2]);
+        TreeNode root = new  TreeNode(null==(nums[(begin+end)/2])?0:nums[(begin+end)/2]);
         root.left = helper(begin,(begin+end)/2-1);
         root.right = helper((begin+end)/2+1,end);
         return root;
+    } 
+	public TreeNode createBinaryTreeByArray(Integer []array,int index)
+    {
+        TreeNode tn = null;
+        if (index<array.length) {
+            Integer value = array[index];
+            if (value == null) {
+                return null;
+            }
+            tn = new TreeNode(value);
+            tn.left = createBinaryTreeByArray(array, 2*index+1);
+            tn.right = createBinaryTreeByArray(array, 2*index+2);
+            return tn;
+        }
+        return tn;
     } 
 }
