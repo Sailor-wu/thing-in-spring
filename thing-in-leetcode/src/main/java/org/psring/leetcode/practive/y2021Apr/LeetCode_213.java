6f01742ae4dc11e4f2beb36cee47b8cc5a0cd879
Sailor-wu <2856346101@qq.com>
2021-04-16 11:37:58 +0800

    打家劫舍1    2.

--- thing-in-leetcode/src/main/java/org/psring/leetcode/practive/y2021Apr/LeetCode_213.java
+++ thing-in-leetcode/src/main/java/org/psring/leetcode/practive/y2021Apr/LeetCode_213.java
@@ -8,7 +8,18 @@ import java.util.Arrays;
 public class LeetCode_213 {
 
 	public static void main(String[] args) {
-		
+		int sum = 0;
+		for (int i = 0; i < 50; i++) {
+			sum=sum+i;
+		}
+		System.out.println(fib(10));
+	}
+	
+	public static int fib(int n) {
+		if(n<2) {
+			return n;
+		}
+		return fib(n-1)+fib(n-2);
 	}
 	
 	public int rob(int[] nums) {
